cca9907cd19e47406804b84fbab4e765ae1ae3dd
CalumLamb1 <90246900+CalumLamb1@users.noreply.github.com>
2025-06-11 16:39:11 +0100

    Update indicators.py

--- core/indicators.py
+++ core/indicators.py
@@ -3,12 +3,12 @@ import ta
 
 def add_indicators(df):
     df = df.copy()
-    df["EMA_50"] = ta.trend.ema_indicator(df["Close"], 50)
-    df["EMA_200"] = ta.trend.ema_indicator(df["Close"], 200)
-    macd, macd_signal, _ = ta.MACD(df["Close"])
+    df["EMA_50"] = ta.trend.ema_indicator(df["Close"].squeeze()
+    df["EMA_200"] = ta.trend.ema_indicator(df["Close"].squeeze()
+    macd, macd_signal, _ = ta.MACD(df["Close"].squeeze()
 df["MACD"] = macd.squeeze()
 df["MACD_signal"] = macd_signal.squeeze()
-    df["MACD_hist"] = ta.trend.macd_diff(df["Close"])
-    df["RSI"] = ta.momentum.rsi(df["Close"], 14)
-    df["ATR"] = ta.volatility.average_true_range(df["High"], df["Low"], df["Close"], 14)
+    df["MACD_hist"] = ta.trend.macd_diff(df["Close"].squeeze()
+    df["RSI"] = ta.momentum.rsi(df["Close"].squeeze()
+    df["ATR"] = ta.volatility.average_true_range(df["High"], df["Low"], df["Close"].squeeze()
     return df
